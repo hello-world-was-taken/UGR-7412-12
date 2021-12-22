1e4e5a09db5115622bd9c83cb4a6c75a51e6149e
hello_world <haileformal@gmail.com>
2021-12-22 00:55:56 +0000

    finish meaning full concepts

--- js/common.js
+++ js/common.js
@@ -1,5 +1,4 @@
 function active() {
-    console.log("inside")
     let loca = location.href
     let list = document.getElementsByClassName("navigation__link")
     let len = list.length
@@ -13,18 +12,34 @@ function active() {
 window.onload = active
 
 function show_nav() {
-    console.log("inside show_nav")
     let nav = document.getElementById("nav")
-    var first = true;
     if (nav.classList[nav.classList.length - 1] == "display__nav") {
-        console.log("inside if")
         nav.classList.remove("display__nav");
-        // nav.classList.add("navigation__list")
-        first = false
       } else {
-        // nav.classList.remove("navigation__list")
         nav.classList.add("display__nav");
-        // active()
-        first = true
       }
+}
+
+// var backend = document.getElementById("back-end")
+// var frontend = document.getElementById("front-end")
+
+function show_hide_back_end() {
+    let back_end = document.getElementById('skills__back-end')
+    if (back_end.style.display === "none") {
+        back_end.style.display = "block"
+    }
+    else {
+        back_end.style.display = "none"
+    }
+}
+
+function show_hide_front_end() {
+    let back_end = document.getElementById('skills__front-end')
+    if (back_end.style.display === "none") {
+        back_end.style.display = "block"
+        back_end.style.transition = "all 2s ease"
+    }
+    else {
+        back_end.style.display = "none"
+    }
 }
